bd952ed657bc5d4a435ac85110b4186c247eec4d
Grayson <GMcClead@sportsbusinessjournal.com>
2024-01-28 13:09:40 -0600

    first prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PostComponent } from './Components/post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
